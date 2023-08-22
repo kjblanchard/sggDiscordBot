7ec7d9eb9f1d38b760c9511923edc95184fe3c16
Kevin Blanchard <blanchardkevinj@gmail.com>
2023-08-22 18:56:15 -0400

    Refac some more

--- discord/discord.go
+++ discord/discord.go
@@ -2,14 +2,14 @@ package discord
 
 import (
 	"github.com/bwmarrin/discordgo"
-	"github.com/kjblanchard/sggDiscordBot/github"
 	"log"
 )
 
 var (
-	s *discordgo.Session
-	discordApplicationId string
+	s                      *discordgo.Session
+	discordApplicationId   string
 	supergoonGamesServerId string
+	sysopRoleId = "907314874101665823"
 )
 
 func InitializeDiscord(token string, appId string, supergoonServerId string) {
@@ -18,78 +18,11 @@ func InitializeDiscord(token string, appId string, supergoonServerId string) {
 	supergoonGamesServerId = supergoonServerId
 }
 
-func AddAllSlashCommands() {
-	_, err := s.ApplicationCommandBulkOverwrite(discordApplicationId, supergoonGamesServerId, []*discordgo.ApplicationCommand{
-		{
-			Name:        "check-repos",
-			Description: "Checks to see all of the repos available",
-		},
-		{
-			Name:        "hello-world",
-			Description: "Showcase of a basic slash command",
-		},
-	})
-	if err != nil {
-		log.Fatal("Error doing the thing", err)
-		// Handle the error
-	}
-	s.AddHandler(func(
-		s *discordgo.Session,
-		i *discordgo.InteractionCreate,
-	) {
-		data := i.ApplicationCommandData()
-		switch data.Name {
-		case "hello-world":
-			err := s.InteractionRespond(
-				i.Interaction,
-				&discordgo.InteractionResponse{
-					Type: discordgo.InteractionResponseChannelMessageWithSource,
-					Data: &discordgo.InteractionResponseData{
-						Content: "Hello world!",
-					},
-				},
-			)
-			if err != nil {
-				log.Fatal("Error registering", err)
-				// Handle the error
-			}
-		case "check-repos":
-			roles := i.Member.Roles
-
-			github.GetAllRepos()
-
-			for _, role := range roles {
-				log.Print(role)
-			}
-			err := s.InteractionRespond(
-				i.Interaction,
-				&discordgo.InteractionResponse{
-					Type: discordgo.InteractionResponseChannelMessageWithSource,
-					Data: &discordgo.InteractionResponseData{
-						Content: "Heres a list of all of Kevins Repos..",
-						Embeds: []*discordgo.MessageEmbed{
-							{
-								Title: "Repo",
-							},
-						},
-					},
-				},
-			)
-			if err != nil {
-				log.Fatal("Error registering", err)
-				// Handle the error
-			}
-		}
-	})
-
-}
-
 func OpenDiscordWebsocketConnection() {
 	err := s.Open()
 	if err != nil {
 		log.Fatal("Error Opening websocket connection for discord bot!\nError: ", err)
 	}
-
 }
 
 func CloseDiscord() {
---
+++ discord/slashCommands.go
@@ -0,0 +1,97 @@
+package discord
+
+import (
+	"github.com/bwmarrin/discordgo"
+	"github.com/kjblanchard/sggDiscordBot/github"
+	"log"
+)
+
+var (
+	allApplicationCommands []*discordgo.ApplicationCommand
+)
+
+func checkIfUserInRole(roleToCheckFor string, roles []string) bool {
+	for _, role := range roles {
+		if role == roleToCheckFor {
+			return true
+		}
+	}
+	return false
+}
+
+func AddCheckReposSlashCommand() {
+	command := &discordgo.ApplicationCommand{
+		Name:        "check-repos",
+		Description: "Checks to see all of the repos available",
+	}
+	allApplicationCommands = append(allApplicationCommands, command)
+	s.AddHandler(func(
+		s *discordgo.Session,
+		i *discordgo.InteractionCreate,
+	) {
+		data := i.ApplicationCommandData()
+		if data.Name != "check-repos" {
+			return
+		}
+		github.GetAllRepos()
+		roles := i.Member.Roles
+		response := &discordgo.InteractionResponseData{}
+		if !checkIfUserInRole(sysopRoleId, roles) {
+			response.Content = "Only Sysop can perform this action"
+		} else {
+			response.Content = "Heres a list of all of Kevins Repos.."
+			response.Embeds = []*discordgo.MessageEmbed{
+				{
+					Title: "Repo",
+				},
+			}
+		}
+		err := s.InteractionRespond(
+			i.Interaction,
+			&discordgo.InteractionResponse{
+				Type: discordgo.InteractionResponseChannelMessageWithSource,
+				Data: response},
+		)
+		if err != nil {
+			log.Fatal("Error responding", err)
+		}
+	})
+}
+
+func AddHelloWorldSlashCommand() {
+	command := &discordgo.ApplicationCommand{
+		Name:        "hello-world",
+		Description: "Showcase of a basic slash command",
+	}
+	allApplicationCommands = append(allApplicationCommands, command)
+	s.AddHandler(func(
+		s *discordgo.Session,
+		i *discordgo.InteractionCreate,
+	) {
+		data := i.ApplicationCommandData()
+		if data.Name == "hello-world" {
+			err := s.InteractionRespond(
+				i.Interaction,
+				&discordgo.InteractionResponse{
+					Type: discordgo.InteractionResponseChannelMessageWithSource,
+					Data: &discordgo.InteractionResponseData{
+						Content: "Hello world!",
+					},
+				},
+			)
+			if err != nil {
+				log.Fatal("Error responding", err)
+			}
+		}
+	})
+}
+
+func AddAllSlashCommands() {
+	AddHelloWorldSlashCommand()
+	AddCheckReposSlashCommand()
+	_, err := s.ApplicationCommandBulkOverwrite(discordApplicationId, supergoonGamesServerId, allApplicationCommands)
+	if err != nil {
+		log.Fatal("Error adding application commands", err)
+	}
+
+}
