50c487eb219454c9e5efc4e3d7b30b41f7b2abbf
Kevin Blanchard <blanchardkevinj@gmail.com>
2023-08-22 09:08:09 -0400

    Handle errors with fails for debugging, and add second interaction and print roles.  Easier to control them here.

--- main.go
+++ main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"encoding/json"
-	"github.com/bwmarrin/discordgo"
 	"log"
 	"os"
 	"os/signal"
+
+	"github.com/bwmarrin/discordgo"
 )
 
 type appSettings struct {
@@ -29,11 +30,16 @@ func main() {
 	s, _ := discordgo.New("Bot " + config.Token)
 	_, err = s.ApplicationCommandBulkOverwrite(config.AppId, config.SupergoonGamesServerId, []*discordgo.ApplicationCommand{
 		{
+			Name:        "check-repos",
+			Description: "Checks to see all of the repos available",
+		},
+		{
 			Name:        "hello-world",
 			Description: "Showcase of a basic slash command",
 		},
 	})
 	if err != nil {
+		log.Fatal("Error doing the thing", err)
 		// Handle the error
 	}
 	s.AddHandler(func(
@@ -53,13 +59,34 @@ func main() {
 				},
 			)
 			if err != nil {
+				log.Fatal("Error registering", err)
+				// Handle the error
+			}
+		case "check-repos":
+			roles := i.Member.Roles
+
+			for _, role := range roles {
+				log.Print(role)
+
+			}
+			err := s.InteractionRespond(
+				i.Interaction,
+				&discordgo.InteractionResponse{
+					Type: discordgo.InteractionResponseChannelMessageWithSource,
+					Data: &discordgo.InteractionResponseData{
+						Content: "I should be checking the repos huh",
+					},
+				},
+			)
+			if err != nil {
+				log.Fatal("Error registering", err)
 				// Handle the error
 			}
 		}
 	})
 	err = s.Open()
 	if err != nil {
-		// Handle the error
+		log.Fatal("Error registering", err)
 	}
 	stop := make(chan os.Signal, 1)
 	signal.Notify(stop, os.Interrupt)
@@ -68,6 +95,6 @@ func main() {
 
 	err = s.Close()
 	if err != nil {
-		// Handle the error
+		log.Fatal("Error registering", err)
 	}
 }
