2d45993e65611e78e3f3ee40fbf93fc4d53c8178
Kevin Blanchard <blanchardkevinj@gmail.com>
2023-08-23 20:26:33 -0400

    try fix

--- terraform/modules/service/service.tf
+++ terraform/modules/service/service.tf
@@ -13,19 +13,7 @@ resource "kubernetes_service" "kubernetes_service_module" {
       content {
         port = port.value.port
         name = port.value.name
-                # Include targetPort only if it exists in port.value
-        dynamic "target_port" {
-          for_each = can(port.value.target_port) ? [1] : []
-          content {
-            target_port = port.value.target_port
-          }
-        }
-        # dynamic "target_port" {
-        #   for_each = port.value.target_port
-        #   content {
-        #     target_port = port.value.target_port
-        #   }
-        # }
+        target_port = can(port.value.target_port) ? port.value.target_port : null
       }
     }
     dynamic "port" {
