6112fccb98176443b0510552b3f4f052ade3c1b0
Kevin Blanchard <blanchardkevinj@gmail.com>
2023-08-24 20:32:15 -0400

    Try again with the proper slash

--- goonHttp/goonHttp.go
+++ goonHttp/goonHttp.go
@@ -10,6 +10,6 @@ import (
 func StartServer() {
 	log.Print("Starting server..")
 	http.HandleFunc("/api/v1/webhooks", handlers.HandleWebhook)
-	http.HandleFunc("api/v1/webhooks/sggDiscordBot", handlers.HandleSupergoonGamesDiscordBot)
+	http.HandleFunc("/api/v1/webhooks/sggDiscordBot", handlers.HandleSupergoonGamesDiscordBot)
 	log.Fatal(http.ListenAndServe(":80", nil))
 }
